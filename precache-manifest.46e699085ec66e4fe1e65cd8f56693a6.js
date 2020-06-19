ab6ef7355c0c73e61e8862a0cf865f9bd262f762
Evgenii Los <evgeniilos@Evgeniis-MBP.lan1>
2020-06-19 16:06:02 -0400

    Updates

---
+++ precache-manifest.46e699085ec66e4fe1e65cd8f56693a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f30b0eb4fb7ad5cd8736ef2727c8301",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3dbf8f7fa50924f46e90",
+    "url": "/tic-tac-toe/static/css/main.5164b08d.chunk.css"
+  },
+  {
+    "revision": "c52c1ba142931b798eac",
+    "url": "/tic-tac-toe/static/js/2.5b397fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.5b397fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbf8f7fa50924f46e90",
+    "url": "/tic-tac-toe/static/js/main.17d48ce9.chunk.js"
+  },
+  {
+    "revision": "297324bde6b16cfafb08",
+    "url": "/tic-tac-toe/static/js/runtime-main.b1fc456d.js"
+  }
+]);
